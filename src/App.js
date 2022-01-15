1112ce068149f790ac781e6b8c0a47687d70f2a9
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-01-16 00:36:56 +0700

    menambahkan blogDDetail

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
+import BlogDetail from './pages/BlogDetail';
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
       <nav>
           <ul>
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="profile">Profile</Link></li>
+            <li><Link to="blog">Blog</Link></li>
+            <li><Link to="blog/:id">BlogDetail</Link></li>
+            <li><Link to="contact">Contact</Link></li>
           </ul>
       </nav>
       {/* Core logic path */}
@@ -21,6 +23,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="profile" element={<Profile />} />
         <Route path="blog" element={<Blog />} />
+        <Route path="blog/:id" element={<BlogDetail />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
     </div>
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Blog() {
     const [articles, setArticles] = useState([]);
@@ -28,7 +29,16 @@ export default function Blog() {
                 {!loading && (
                     <div>
                         {articles.map(function(article) {
-                            return <article>{article.title}</article>;
+                            return(
+                                <article key={article.id}>
+                                    <h2>
+                                        <Link to={`/blog/${article.id}`}>{article.title}></Link>
+                                    </h2>
+                                    <date>
+                                        {new Date(article.publishedAt).toLocaleDateString()}
+                                    </date>
+                                </article>
+                            )
                         })};
                     </div>
                 )};
